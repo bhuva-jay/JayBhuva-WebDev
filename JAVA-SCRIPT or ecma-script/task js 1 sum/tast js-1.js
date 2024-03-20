1ce941f8bf703e164826ad0f38c217d13d578f5d
jay-bhuva <sq.jaybhuva@gmail.com>
2024-03-20 12:09:08 +0530

    20-03-24

---
+++ JAVA-SCRIPT or ecma-script/task js 1 sum/tast js-1.js
@@ -0,0 +1,79 @@
+let a =10;
+let b =50;
+let c = a+b;
+ console.log(c)
+
+
+{
+    let p =1;
+    let r= -1;
+      if(p>r) {
+               console.log( "p is a positive");
+      }
+      else{
+       console.log("r is a negative ");
+   
+      }
+   
+      let year =2005;
+      if( year % 5 == 0) {
+       console.log("leap year");
+      } else {
+       console.log("not leap year");
+      }
+}
+ 
+{
+let  age = 21
+let votaingcard =true;
+ if (age<=18) {
+    console.log("you are a not aligible for voting card ");
+ } else {
+        if( votaingcard == true ){
+            console.log("are you aligible for voting card");
+        }
+        else{
+            console.log("you are not aligible to voting card");
+        }
+ }    
+}
+
+
+{
+    let score =25;
+    if (score>=70) {
+       console.log("a grade"); 
+    }
+    else if( score >=80&&score <=90){
+        console.log("b grade");
+    }
+    else if( score >=60&&score <=70){
+        console.log(" c grade");
+    }
+    else if( score >=50&&score <=55){
+        console.log(" d grade");
+    }
+    else{
+        console.log("you are flied");
+    }
+   
+}
+
+{
+    let m = 6;
+    let n = 5;
+    let o = 3;
+     if (m>n&&m>o) {
+        console.log(m + " is a greater");
+     }
+     else if (n>m&&n>o) {
+        console.log(n + " is a greater");
+     }
+     else  {
+        console.log(o + " is a greater");
+     }
+}
+
+{
+    
+}
